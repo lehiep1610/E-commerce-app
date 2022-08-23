7f485d7ed7d26112d329122ff62055348ac2db72
lehiep1610 <hieple.1606@gmail.com>
2022-08-23 12:19:10 +0700

    Update UI for account screen

--- lib/features/account/widgets/orders.dart
+++ lib/features/account/widgets/orders.dart
@@ -1,4 +1,5 @@
 import 'package:e_commerce_app/constants/global_variables.dart';
+import 'package:e_commerce_app/features/account/widgets/single_product.dart';
 import 'package:flutter/material.dart';
 
 class Orders extends StatefulWidget {
@@ -9,6 +10,13 @@ class Orders extends StatefulWidget {
 }
 
 class _OrdersState extends State<Orders> {
+  List list = [
+    'https://i.ibb.co/PccVw0r/iphone-13-pro-max-sierra-blue-600x600.jpg',
+    'https://i.ibb.co/XpPWFs8/oppo-reno7-5g-den-thumb-1-600x600.jpg',
+    'https://i.ibb.co/6vw1Z36/oppo-reno8z-5g-den-thumb-600x600.jpg',
+    'https://i.ibb.co/NYXQS6k/samsung-galaxy-a33-5g-xanh-thumb-600x600.jpg',
+    'https://i.ibb.co/yB8gR68/samsung-galaxy-z-fold4-120822-014401-600x600.jpg',
+  ];
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -36,6 +44,17 @@ class _OrdersState extends State<Orders> {
             ),
           ],
         ),
+        Container(
+          height: 170,
+          padding: const EdgeInsets.only(left: 10, right: 20),
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: list.length,
+            itemBuilder: (context, index) {
+              return SingleProduct(image: list[index]);
+            },
+          ),
+        )
       ],
     );
   }
